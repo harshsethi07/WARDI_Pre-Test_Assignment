051fd72e0d3616385e5f3ed2cf9d4e9aab7f7fa5
Harshpreet singh <harshpreet15038@iiitd.ac.in>
2018-08-06 00:32:14 +0530

    Add files via upload
    Arduino Code and Circuit Diagram

---
+++ WARDI_Assignment.ino
@@ -0,0 +1,26 @@
+/*
+  
+  REFRENCES:
+  https://www.arduino.cc/en/tutorial/potentiometer
+  https://programmingelectronics.com/tutorial-20-analog-io-and-serial-communications-old-version/
+
+  The Circuit Schematic was also taken from the above links.
+*/
+
+
+int potPin = 0;    // potentiometer input
+int ledPin = 9;   
+int val = 0;        
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  
+}
+
+void loop() {
+  val = analogRead(potPin);   // read potentiometer input
+  
+  if ( val > 512 )    // 512 equals 2.5V since 1024 is equivalent to 5V
+    digitalWrite(ledPin, HIGH);   // turn on the led
+  else
+    digitalWrite(ledPin, LOW);    // turn off the led    
+}
